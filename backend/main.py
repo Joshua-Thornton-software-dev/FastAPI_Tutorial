cb54b16ae0d8d8e5e40b239fdef190774c8ce919
Josh Thor <JoshuaThornton.SoftwareDev@Gmail.com>
2025-11-17 13:16:50 -0500

    Allow redoc to work on Chrome
    Chrome doesn't like pulling the necessary js file for redoc to work due to CORS restrictions, so I am now hosting that file as a static asset and overriding fastAPI's default redoc path to my own.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+# from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.docs import get_redoc_html
+from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
+app = FastAPI(redoc_url=None, docs_url="/docs")
+
+# Mount static files.
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Chrome doesn't like getting the redoc js file from the normal URL, so I am hosting it as a static asset.
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js"
+    )
 
 @app.get("/")
 async def root():
